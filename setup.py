875659f7e14b8d1c4af492f0905ef73508ee4a5e
Piero Naldesi <naldesi@bo.infn.it>
2018-09-17 20:46:21 +0200

    uga

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-	ext_modules = cythonize('function.py', 'observables.py', 'hamiltonian.py')
+	ext_modules = cythonize('bose.py', 'hamiltonian.py', 'function.py', 'observables.py')
 )
 
 # python setup.py build_ext --inplace
