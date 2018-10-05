784797e60e8fd5bb4200fbb27abe666ce7bb25e3
Juan Polo Gomez <zestzestzest@gmail.com>
2018-10-05 14:39:19 +0200

    manual

---
+++ manual/scripts/function.py
@@ -0,0 +1,216 @@
+import numpy as np
+import itertools
+import os
+from scipy.sparse import csc_matrix
+import time
+
+
+#..................................hilbert space dimension
+def fact_creation(**args):
+
+	ll = args.get("ll")
+	nn = args.get("nn")	
+
+	tab = [1 for x in range(nn+ll+2)]
+
+	for i in range(nn+ll-1):
+		tab[i+1] = int(tab[i]*(i+1))
+
+	return tab
+
+
+def hilb_dim(x,y,tab_fact):
+	
+	#x = args.get("nn")
+	#y = args.get("ll")	
+	#tab_fact = args.get("tab_fact")
+
+	kk  = tab_fact[x+y-1]/(tab_fact[x]*tab_fact[y-1])
+
+	uga = int(kk)
+	
+	return uga
+
+def hilb_dim_tab(**args):
+
+	ll = args.get("ll")
+	nn = args.get("nn")	
+	tab_fact = args.get("tab_fact")
+
+	tab = [[hilb_dim(n, l, tab_fact) for l in range(ll+1)] for n in range(nn+1)]
+
+	return tab
+
+#..................................base preparation
+
+def Base_prep(**args):
+
+	ll = args.get("ll")
+	nn = args.get("nn")
+
+	base_bin = []
+	base_num = []
+	base_ind = []
+
+#	Max = int(sum([2**(ll+nn-2-x) for x in range(nn)]))
+#	TO_con_tab = [None] * Max
+
+	for bits in itertools.combinations(range(nn+ll-1), nn):
+		s = ['0'] * (nn+ll-1)	
+		for bit in bits:
+			s[bit] = '1'	
+		bi = ''.join(s)	
+		base_bin.append(bi)
+
+		bose = TO_bose_conf(s,ll)
+
+		base_num.append(bose)
+
+		in_bi=int(bi,2)
+		base_ind.append(in_bi)
+
+		#TO_con_tab[in_bi] = bi
+
+	base_bose = np.asarray(base_num, dtype=np.int8)
+
+	return base_bin, base_bose, base_ind
+
+
+#..................................index search
+def get_index(state,**args):
+
+	ll           = args.get("ll")
+	nn           = args.get("nn")
+	DIM_H        = args.get("DIM_H")
+	tab_fact     = args.get("tab_fact")
+	hilb_dim_tab = args.get("hilb_dim_tab")
+
+	size   = int(nn+ll-1)
+	r_par  = int(nn)    #remaining_particles
+	r_sit  = int(ll)	#remaining_sites
+	result = DIM_H
+
+	for jj in range(size):
+		action_i = int(state[jj]);
+
+		if r_par==0:
+			break
+		if action_i == 0:
+			#print(jj,r_par)		
+		
+			result -= hilb_dim_tab[r_par-1][r_sit]
+			#result -= binomial_table[r_par-1][r_sit]
+			r_sit  -= 1;
+
+		else:
+			r_par -= 1;
+			#print('else',jj,r_par)
+
+	return DIM_H-result
+
+#..................................from BOSE configuration to bin number
+def FROM_bose_TO_bin(state,**args):
+
+	i=[]
+
+	for x in state:
+		if x == 0:
+			i.append('0')
+		else:
+			for x in range(x):
+				i.append('1')
+			i.append('0')
+
+	i   = i[:-1]
+	uga = ''.join(i)
+
+	return uga
+
+
+
+
+#..................................from configuration to bin number
+def TO_bin(xx):
+	return int(xx,2)
+
+#..................................from bin number to configuration
+def TO_con(x,L):
+	x1=int(x)
+	L1=int(L)
+	bi=np.binary_repr(x1, width=L1)
+
+#	print(x1,bi)
+
+	return  bi
+
+#def TO_con_1(x,L):
+	
+#	ind = CONF_tab_sp[x]
+
+	#ind0 = CONF_tab_sp[x].toarray()
+	#ind  = ind0[0,0]
+
+#	return BASE_bin[ind]
+
+#..................................hop. preparation
+#............. ......BC=0 periodic, BC=1 open
+def Hop_prep(**args):
+
+	X = args.get("ll")
+	Y = args.get("nn")
+
+	Hop_dim=X+Y-2
+	
+	return [TO_con(2**i+2**((i+1)%(X+Y-1)),X+Y-1) for i in range(Hop_dim)]
+
+#..................................counting number of one
+POPCOUNT_TABLE16 = [0] * 2**16
+for index in range(len(POPCOUNT_TABLE16)):
+	POPCOUNT_TABLE16[index] = (index & 1) + POPCOUNT_TABLE16[index >> 1]
+
+def one_count(v):
+	return (POPCOUNT_TABLE16[ v        & 0xffff] +
+			POPCOUNT_TABLE16[(v >> 16) & 0xffff])
+
+def TO_bose_conf(x,ll):
+	p=0
+	conf = np.zeros(ll, dtype=np.int)
+	for jj in range(len(x)):
+		if x[jj]=='1':
+			conf[p]+=1
+		else: 
+			p+=1	
+
+	return 	conf
+
+
+#..................................................generate filename
+def generate_filename(basename):
+	local_time = str(int(round(time.time() * 1000)))
+	xx = basename + local_time + ".dat"
+	if os.path.isfile(xx):
+		time.sleep(1)
+		return generate_filename(basename)
+	return xx
+
+#..................................................Traslations MEAN
+def Trasl_Mean(A):
+	a = A.shape
+	B = np.zeros((a[1],a[1]), dtype=np.float)
+	for i in range(a[1]):
+		B[i] = np.roll(A[i],-i)
+	return np.mean(B, axis=0)
+
+
+#..................................................Print_MATRIX
+def print_matrix(H):
+
+	#print('matrix to print')
+
+	if isinstance(H, csc_matrix):
+		print_h = csc_matrix.todense(H)
+		print(print_h)
+	else:
+		print(H)
+
+	return 0
---
+++ manual/scripts/hamiltonian.py
@@ -0,0 +1,245 @@
+import numpy as np
+from scipy.sparse import csc_matrix
+from scipy.sparse import linalg as linalgS
+from numpy import linalg as lin
+
+import function as ff
+import hamiltonian_parity as ham_par
+from joblib import Parallel, delayed
+
+
+
+def bose_Hamiltonian (**args):
+
+#Hamiltonian needs as input:
+
+#...... Parameter: DIM_H
+
+	DIM_H 	 = np.int(args.get("DIM_H"))
+
+#Hamiltonian returns:
+#...... Sparse or Dense matrix. By default is SPARSE
+	
+	mat_type = args.get("mat_type")
+	if mat_type == None:
+		mat_type = 'Sparse'
+
+
+#...... Creates i,j,H[i,j]
+
+	cores_num = np.int(args.get("cores_num"))
+
+	if cores_num > 1:
+		
+		Hamiltonian = csc_matrix(([], ([], [])), shape=(DIM_H,DIM_H), dtype=np.double)
+		
+		step = DIM_H // cores_num
+		split = Parallel(n_jobs=cores_num)(delayed(parallel_evaluate_ham)(step*k, step*(k+1),**args) for k in range(cores_num))
+
+		for i in range(cores_num):
+			
+			Hamiltonian += split[i]
+
+	else:
+
+		X0 = []
+		Y0 = []
+		A0 = []
+
+		for i in range(DIM_H):
+
+			A, B, C = evaluate_ham(i,**args)
+			
+			X0.append(A)
+			Y0.append(B)
+			A0.append(C)
+
+		#here we flatten the arrays
+		X1 = [item for sublist in X0 for item in sublist]
+		Y1 = [item for sublist in Y0 for item in sublist]
+		A1 = [item for sublist in A0 for item in sublist]
+
+		Hamiltonian = csc_matrix((A1, (X1,Y1)), shape=(DIM_H,DIM_H), dtype=np.double)
+
+
+	if mat_type == 'Dense':
+
+		Hamiltonian = csc_matrix.todense(Hamiltonian)
+
+
+	#ff.print_matrix(Hamiltonian)
+
+
+	return Hamiltonian
+
+
+
+
+def parallel_evaluate_ham (a,b,**args):
+	
+	DIM_H      = args.get("DIM_H")
+	Hamiltonian = csc_matrix(([], ([], [])), shape=(DIM_H,DIM_H), dtype=np.double)
+	
+	for i in range(DIM_H):
+		A, B, C = evaluate_ham(i,**args)
+		Hamiltonian += csc_matrix((C, (A,B)), shape=(DIM_H,DIM_H), dtype=np.double)
+
+	return Hamiltonian
+
+
+
+
+def evaluate_ham(i,**args):
+
+#...... Parameter: BC, t, U, DIM_H, nn, ll
+
+	nn 		 = args.get("nn")
+	ll 		 = args.get("ll")	
+	BC 		 = args.get("BC")
+	t 		 = args.get("t")	
+
+#...... Functions in ham.py: action_hopping, action_interactions
+#...... Fundamental tables:  BASE_bin, HOP_list
+
+	BASE_bin = args.get("BASE_bin")
+	HOP_list = args.get("HOP_list")
+
+
+#Hamiltonian returns:
+#...... Sparse or Dense matrix. By default is SPARSE
+	
+	mat_type = args.get("mat_type")
+	if mat_type == None:
+		mat_type = 'Sparse'
+
+	ham_ind1 = []
+	ham_ind2 = []
+	ham_val  = []
+
+	state = BASE_bin[i]
+
+##----- INTERACTIONS
+
+	int_val = action_interactions(state,**args)
+
+	if int_val != 0.0:
+	
+		#---- INTERACTION = we store i,i,int_val !!!!
+		ham_ind1.append( i )
+		ham_ind2.append( i )
+		ham_val.append( int_val )
+
+##----- KINETIC
+	for hop in HOP_list:
+		hop_state_bin = ff.TO_bin(state)^ff.TO_bin(hop)
+		
+		# we cut states with not N particles
+		if ff.one_count(hop_state_bin) == nn:
+
+			#hop_state     = TO_con_tab[hop_state_bin]
+			hop_state     = ff.TO_con(hop_state_bin,ll+nn-1)
+
+			j = ff.get_index(hop_state,**args)	
+
+			kin_val = t*action_hopping(i,j,**args)
+
+			if kin_val != 0.0:
+
+	#---- KINETIC = we store i,j,kin_val !!!!
+				ham_ind1.append( i )
+				ham_ind2.append( j )
+				ham_val.append( kin_val )
+
+	# here we put the PERIODIC
+	#...................BC=0 periodic, BC=1 open
+
+	if BC == 0:
+		if state[0] == '1':
+
+			PBC_newstate1 = state[1:]+state[0]				
+			j = ff.get_index(PBC_newstate1,**args)	
+
+			kin_val = t*action_hopping(i,j,**args)
+			
+			if kin_val != 0.0:
+
+	#---- KINETIC = we store i,j,kin_val !!!!
+				ham_ind1.append( i )
+				ham_ind2.append( j )
+				ham_val.append( kin_val )
+
+		if state[-1] == '1':
+
+			PBC_newstate2 = state[-1]+state[:-1]
+			j = ff.get_index(PBC_newstate2,**args)	
+
+			kin_val = t*action_hopping(i,j,**args)
+			
+			if kin_val != 0.0:
+
+	#---- KINETIC = we store i,j,kin_val !!!!
+				ham_ind1.append( i )
+				ham_ind2.append( j )
+				ham_val.append( kin_val )
+
+	return ham_ind1, ham_ind2, ham_val
+
+
+
+def action_hopping(x,y,**args):
+
+	ll 		 = args.get("ll")	
+	BASE_bin = args.get("BASE_bin")
+
+
+	state_x   = BASE_bin[x]
+	bosecon_x = ff.TO_bose_conf(state_x,ll)
+
+	state_y   = BASE_bin[y]
+	bosecon_y = ff.TO_bose_conf(state_y,ll)
+
+	jump_c  =	np.argmax(bosecon_x-bosecon_y)
+	jump_cd  =	np.argmin(bosecon_x-bosecon_y)
+
+	result	  = np.sqrt((bosecon_x[jump_cd]+1)*(bosecon_x[jump_c])) 
+	
+	return result
+
+
+def action_interactions(state,**args):
+
+	ll 		 = args.get("ll")
+	U 		 = args.get("U")
+
+	bosecon = ff.TO_bose_conf(state,ll)
+	int_val = 0.5*U*np.dot(bosecon,bosecon-1.)
+
+	return int_val
+
+def diagonalization(Hamiltonian,**args):
+
+	DIM_H    = args.get("DIM_H")
+	mat_type = args.get("mat_type")
+	parity	 = args.get("parity")
+	num_eig	 = args.get("n_diag_state")
+
+	if num_eig >= DIM_H:
+		num_eig = DIM_H-2
+
+	if mat_type == 'Sparse':
+
+		A,B = linalgS.eigsh(Hamiltonian, k=num_eig, which='SA', return_eigenvectors=True)
+
+	else:
+
+		A,B   = lin.eigh(Hamiltonian)
+
+	if parity == 'True':
+		B = ham_par.vectors_parity_symmetrize(B,**args)
+
+	return A,B
+
+
+
+
+
