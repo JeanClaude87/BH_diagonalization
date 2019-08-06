f1f179ef017e69eeb2cd731800b5b3a30662cb6b
Piero Claude <naldesi@bo.infn.it>
2019-08-06 19:31:50 +0200

    iga

--- bose.py
+++ bose.py
@@ -51,13 +51,14 @@ for nn_inp in [2,3,4,5,6]:
 		
 	for bar_inp in [0.05, 0.03, 0.01, 0.007, 0.005, 0.003, 0.001, 0.0007, 0.0005, 0.0003, 0.0001]:
 	
-	'''
+		'''
 		if nn_inp == 2:	bar_inp = 0.007
 		if nn_inp == 3:	bar_inp = 0.003
 		if nn_inp == 4:	bar_inp = 0.001	
 		if nn_inp == 5:	bar_inp = 0.001	
 		if nn_inp == 6:	bar_inp = 0.0007			
-	'''
+		'''
+		
 		flux_inp 		= 0.0
 		flux_inp_1 		= 1.0
 		flux_inp_psi0 	= 0.0
